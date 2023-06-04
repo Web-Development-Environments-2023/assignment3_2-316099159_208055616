5c6ae23856d1b92f44244150bdcb9673fbaf74c0
Omer Hanan <omer.hanan@gmail.com>
2023-06-04 21:26:09 +0300

    Update to zeros

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -209,7 +209,7 @@ const argumentsValidation = (req, res, next) => {
 async function generateNewId()
 {
     let result = await DButils.execQuery("SELECT MAX(id) as id FROM latestindex");
-    newId = result[0].id + 1;
+    newId = "000" + result[0].id + 1;
     console.log("Recipe new Id is: " + newId);
     await DButils.execQuery(`INSERT INTO latestindex VALUES ('${newId}')`);
     return newId;
