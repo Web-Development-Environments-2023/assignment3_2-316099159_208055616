978368eae6c1a689c3b42ebf8cfb34234e850dc2
Omer Hanan <omer.hanan@gmail.com>
2023-06-04 20:52:14 +0300

    update utils

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -98,7 +98,7 @@ async function addNewRecipe(r) {
     await DButils.execQuery(
         `INSERT INTO recipes VALUES ('${id}', '${r.title}', '${r.image}', '${r.readyInMinutes}', '${r.popularity}', '${vegetarian}', '${vegan}', '${glutenFree}')`
     );
-    await user_utils.addToMyRecipes(req.session.user_id, id);
+    await users_utils.addToMyRecipes(req.session.user_id, id);
     return r;
 }
 
